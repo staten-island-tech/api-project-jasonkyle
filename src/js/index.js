0f7f707c6c27789042cab4f32dfecead9587c5d3
JasonC123456789 <71746733+JasonC123456789@users.noreply.github.com>
2021-05-17 12:33:32 -0400

    rip

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
-import { DOMSelectors } from "./DOM";
+import { DOMSelectors } from "./";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "";
+
+const query = async function () {
+    try {
+      const response = await fetch(
+        `https://api.jikan.moe/v3/anime/1/episodes/2`
+      );
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+      alert("Hey, something went wrong!");
+    }
+  };
+  query();
+
+//Search Bar
+//Filter Results 
