42bfbf834b46667725184e16612116fb629d03c0
JasonC123456789 <71746733+JasonC123456789@users.noreply.github.com>
2021-05-20 01:24:18 -0400

    ehe

--- src/js/index.js
+++ src/js/index.js
@@ -6,17 +6,27 @@ const key = "";
 const query = async function () {
     try {
       const response = await fetch(
-        `https://api.jikan.moe/v3/anime/1/episodes/2`
+        `https://api.jikan.moe/v3/top/anime/1/airing`
       );
       const data = await response.json();
       console.log(data);
-      return data;
+      data.results.forEach((anime) => {
+        DOMSelectors.grid.insertAdjacentHTML(
+          "beforeend",
+          `<div class="release">
+          <img src="" alt="" class="release">
+        </div>
+        <div class="infograph">
+          <h1 class="anime-title">{$anime.title}</h1>
+          <h2 class="anime-rating">{$anime.score}</h2>
+          <h3 class="anime-ep">{anime.episodes<h3>
+          <h4 class="anime-date"{$anime.start_date}></h4>
+          <h5 class="anime-genre">{$anime.}</h5>`
+        );
+      });
     } catch (error) {
       console.log(error);
       alert("Hey, something went wrong!");
-    }
-  };
-  query();
-
-//Search Bar
-//Filter Results 
+    
+};
+  query()};
